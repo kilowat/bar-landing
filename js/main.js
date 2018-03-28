c8eb2fc01b4ff6633ab99cbc8d3e5c1965d35be6
Kilowat <kadantsev.sergey@gmail.com>
2018-03-28 23:59:04 +0300

    init

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+  progressively.init({
+    delay: 50,
+    throttle: 300,
+    smBreakpoint: 600,
+    onLoad: function(elem) {
+
+    },
+    onLoadComplete: function() {
+
+    }
+  });
+  Revealator.effects_padding = '-500';
+});
